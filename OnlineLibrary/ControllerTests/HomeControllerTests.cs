803d14fcee68fc87bc6cd6d734a077c8a9d46d9e
AkioTokugawa <pretsel.roman1996@gmail.com>
2016-06-01 05:45:30 +0300

    add tests

---
+++ OnlineLibrary/ControllerTests/HomeControllerTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OnlineLibrary.Controllers;
+using Moq;
+using Services;
+using ModelServices;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using Core.ViewModels;
+
+namespace ControllerTests
+{
+    [TestClass]
+    public class HomeControllerTests
+    {
+        private HomeController _controller;
+        private Mock<IUserService> _userServiceMock;
+        private Mock<IBookViewModelService> _bookViewModelServiceMock;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _userServiceMock = new Mock<IUserService>();
+            _bookViewModelServiceMock = new Mock<IBookViewModelService>();
+
+            _bookViewModelServiceMock.Setup(s => s.SearchBook("search")).Returns(new List<SearchBookViewModel>());
+
+            _controller = new HomeController(_userServiceMock.Object, _bookViewModelServiceMock.Object);
+        }
+
+        [TestMethod]
+        public void SearchBookTest()
+        {
+            var result = _controller.BookSearch("search") as ViewResult;
+            _bookViewModelServiceMock.Verify(s => s.SearchBook("search"), Times.Once);
+            Assert.AreEqual("Search", result.ViewName);
+        }
+    }
+}
